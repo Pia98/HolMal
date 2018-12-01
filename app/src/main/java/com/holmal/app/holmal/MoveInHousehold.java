53d25780f85a46b3fbb59a6ecdbc3052ecb87028
Manuela <ma.eska@campus.lmu.de>
2018-12-01 08:16:20 +0100

    Basic layout

---
+++ app/src/main/java/com/holmal/app/holmal/MoveInHousehold.java
@@ -0,0 +1,13 @@
+package com.holmal.app.holmal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoveInHousehold extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_in_household);
+    }
+}
