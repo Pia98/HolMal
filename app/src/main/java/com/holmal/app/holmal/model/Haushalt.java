8f362d700dcc909b8f527bd1246fa8749a3abbfd
Pia Probst <pia.probst@online.de>
2018-12-03 16:19:30 +0100

    adding attributes to Haushalt and Person

--- app/src/main/java/com/holmal/app/holmal/model/Haushalt.java
+++ app/src/main/java/com/holmal/app/holmal/model/Haushalt.java
@@ -1,4 +1,47 @@
 package com.holmal.app.holmal.model;
 
 public class Haushalt {
+    private String haushaltName;
+    private String id;
+    private Person[] personenImHaushalt;
+    private Einkaufsliste[] einkaufslisten;
+
+    public Haushalt(String haushaltName, String id, Person[] personenImHaushalt, Einkaufsliste[] einkaufslisten) {
+        this.haushaltName = haushaltName;
+        this.id = id;
+        this.personenImHaushalt = personenImHaushalt;
+        this.einkaufslisten = einkaufslisten;
+    }
+
+    public String getHaushaltName() {
+        return haushaltName;
+    }
+
+    public void setHaushaltName(String haushaltName) {
+        this.haushaltName = haushaltName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Person[] getPersonenImHaushalt() {
+        return personenImHaushalt;
+    }
+
+    public void setPersonenImHaushalt(Person[] personenImHaushalt) {
+        this.personenImHaushalt = personenImHaushalt;
+    }
+
+    public Einkaufsliste[] getEinkaufslisten() {
+        return einkaufslisten;
+    }
+
+    public void setEinkaufslisten(Einkaufsliste[] einkaufslisten) {
+        this.einkaufslisten = einkaufslisten;
+    }
 }
--- app/src/main/java/com/holmal/app/holmal/model/Person.java
+++ app/src/main/java/com/holmal/app/holmal/model/Person.java
@@ -1,4 +1,39 @@
 package com.holmal.app.holmal.model;
 
 public class Person {
+    private String personName;
+
+    /**@color */
+    private String color;
+    private Haushalt[] haushalteEinerPerson;
+
+    public Person(String personName, String color, Haushalt[] haushalteEinerPerson) {
+        this.personName = personName;
+        this.color = color;
+        this.haushalteEinerPerson = haushalteEinerPerson;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Haushalt[] getHaushalteEinerPerson() {
+        return haushalteEinerPerson;
+    }
+
+    public void setHaushalteEinerPerson(Haushalt[] haushalteEinerPerson) {
+        this.haushalteEinerPerson = haushalteEinerPerson;
+    }
 }
