bc839611332886d6768fa79e9eb5efaf1d893cd0
Svenja Schött <s.schoett@campus.lmu.de>
2018-12-09 16:34:19 +0100

    validation, dass Item eine Bezeichnung hat, bevor es erstellt werden kann. typos gefixt. Versucht in CreateItem reinzukommen.

--- app/src/main/java/com/holmal/app/holmal/CreateItem.java
+++ app/src/main/java/com/holmal/app/holmal/CreateItem.java
@@ -2,6 +2,10 @@ package com.holmal.app.holmal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.OnClick;
 
 public class CreateItem extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class CreateItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_item);
+    }
+
+    @OnClick (R.id.add)
+    public void itemCreationOnClicked(){
+        if(validate()){
+            //TODO implement functionality aka actually add item to list
+            //maybe also check whether item is a favourite here and add to favourites
+        }
+    }
+
+    //Validation: add button can only be pressed if a name has been given
+    public Boolean validate(){
+        EditText itemInput = findViewById(R.id.whatInput);
+        String itemName = itemInput.getText().toString();
+        if (itemName.isEmpty()){
+            Toast.makeText(this, R.string.ErrorEnterItem, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
