96ac810f19c0d48b1293397dec4995b38647056f
Manuela <ma.eska@campus.lmu.de>
2018-12-10 13:01:47 +0100

    added shopping list activity

---
+++ app/src/main/java/com/holmal/app/holmal/AllShoppingLists.java
@@ -0,0 +1,22 @@
+package com.holmal.app.holmal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AllShoppingLists extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_shopping_lists);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.addShoppingList)
+    public void addShoppingListClicked(){
+        // TODO
+    }
+}
