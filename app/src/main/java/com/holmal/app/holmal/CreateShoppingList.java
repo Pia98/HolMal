c93a8fa58ed66e9969cc2039a24b0ddc5bffd947
Manuela <ma.eska@campus.lmu.de>
2018-12-10 15:23:16 +0100

    changed EditText to Spinner in createShoppingList activity

--- app/src/main/java/com/holmal/app/holmal/CreateShoppingList.java
+++ app/src/main/java/com/holmal/app/holmal/CreateShoppingList.java
@@ -1,7 +1,9 @@
 package com.holmal.app.holmal;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
 
 public class CreateShoppingList extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class CreateShoppingList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_shopping_list);
+        ButterKnife.bind(this);
     }
 }
