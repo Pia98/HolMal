162fd02b5dac46a3ccb362e2bd5372b4e134da30
Svenja Schött <s.schoett@campus.lmu.de>
2019-01-24 11:00:09 +0100

    javadoc

--- app/src/main/java/com/holmal/app/holmal/utils/SettingsAdapter.java
+++ app/src/main/java/com/holmal/app/holmal/utils/SettingsAdapter.java
@@ -2,7 +2,6 @@ package com.holmal.app.holmal.utils;
 
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.res.ResourcesCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,22 +9,24 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.google.firebase.database.FirebaseDatabase;
 import com.holmal.app.holmal.R;
 import com.holmal.app.holmal.model.Person;
-
 import java.util.HashMap;
 
-
+/**
+ * Adapter for the SettingsActivity that fills the list of people in this household.
+ */
 public class SettingsAdapter extends ArrayAdapter<Person> {
-
 
     private Context context;
     private HashMap<String, Person> personInHousehold;
     private String[] keys;
 
-    //constructor
+    /**
+     * Constructor. Uses the layout for a single household member as the base for the display of the household members.
+     * @param context SettingsActivity context
+     * @param personInHousehold List of people in the household
+     */
     public SettingsAdapter(Context context, HashMap<String, Person> personInHousehold) {
         super(context, R.layout.single_household_member_settings);
         this.context = context;
@@ -34,6 +35,10 @@ public class SettingsAdapter extends ArrayAdapter<Person> {
     }
 
 
+    /**
+     * Method that gets the amount of people in the household that need to be displayed.
+     * @return household size
+     */
     @Override
     public int getCount() {
         Log.i("personInHousehold", "count: " + personInHousehold);
@@ -44,12 +49,23 @@ public class SettingsAdapter extends ArrayAdapter<Person> {
         }
     }
 
+    /**
+     * Getter for the person in the household
+     * @param i position of the person in the list
+     * @return person
+     */
     @Override
     public Person getItem(int i) {
         return personInHousehold.get(keys[i]);
     }
 
-    //method that actually adapts the view to show the household members
+    /**
+     * Method that adapts the view to show the household members names and colours
+     * @param position of the person in the list
+     * @param convertView view the adapter displays the information on
+     * @param parent parent viewGroup
+     * @return the view of each single row
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
 
