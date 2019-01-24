93a5a8c8f38ca4d57bb2a82162b4fdda1af9d926
Manuela <ma.eska@campus.lmu.de>
2019-01-24 19:26:22 +0100

    myTasks is displayed

--- app/src/main/java/com/holmal/app/holmal/MyTasksActivity.java
+++ app/src/main/java/com/holmal/app/holmal/MyTasksActivity.java
@@ -3,25 +3,45 @@ package com.holmal.app.holmal;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.holmal.app.holmal.model.Item;
+import com.holmal.app.holmal.model.Person;
+import com.holmal.app.holmal.utils.ItemsAdapter;
 import com.holmal.app.holmal.utils.PreferencesAccess;
+
+import java.util.HashMap;
 
 import butterknife.ButterKnife;
 
 //class that shows the users assignments
 public class MyTasksActivity extends AppCompatActivity {
+    private static final String TAG = MyTasksActivity.class.getName();
 
     private DrawerLayout mDrawerLayout;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView list;
+
+    HashMap<String, Item> myItems = new HashMap<>();
+    private HashMap<String, Person> person = new HashMap<>();
+    String householdId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +49,42 @@ public class MyTasksActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_tasks);
         ButterKnife.bind(this);
 
+        list = findViewById(R.id.list);
+        layoutManager = new LinearLayoutManager(this);
+        list.setLayoutManager(layoutManager);
+
+
+        PreferencesAccess preferencesAccess = new PreferencesAccess();
+        String personID = preferencesAccess.readPreferences(this, getString(R.string.personIDPreference));
+        householdId = preferencesAccess.readPreferences(this, getString(R.string.householdIDPreference));
+
         //menu that appears from the left
         menu();
+
+        startPersonListener();
+
+        FirebaseDatabase.getInstance().getReference().child("item").orderByChild("itsTask").equalTo(personID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                myItems.clear();
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    String id = child.getKey();
+                    Item value = child.getValue(Item.class);
+                    Log.i(TAG, "item: " + value);
+                    myItems.put(id, value);
+                }
+                Log.i(TAG, "myItems List " + myItems);
+                //adapter
+                ItemsAdapter adapter = new ItemsAdapter(MyTasksActivity.this, myItems, person);
+                RecyclerView list = findViewById(R.id.list);
+                list.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void menu() {
@@ -107,6 +161,30 @@ public class MyTasksActivity extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    /**
+     * Method that starts the person listener. Gets a list of the people in the active household from the firebase database.
+     */
+    private void startPersonListener() {
+        FirebaseDatabase.getInstance().getReference().child("person").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                person.clear();
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    String id = child.getKey();
+                    Person value = child.getValue(Person.class);
+                    if (value.getIdBelongingTo().equals(householdId)) {
+                        person.put(id, value);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     //Menu is opened
