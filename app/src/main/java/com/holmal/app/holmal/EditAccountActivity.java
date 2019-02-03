b48953db1edd00e5c8a3cedce14720573cf7efe6
Pia Probst <pia.probst@online.de>
2019-02-03 17:01:53 +0100

    03-02-2019 17:01 - signing user in

--- app/src/main/java/com/holmal/app/holmal/EditAccountActivity.java
+++ app/src/main/java/com/holmal/app/holmal/EditAccountActivity.java
@@ -1,11 +1,21 @@
 package com.holmal.app.holmal;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,6 +23,9 @@ import butterknife.OnClick;
 
 public class EditAccountActivity extends AppCompatActivity {
 
+    private static final String TAG = EditAccountActivity.class.getName();
+
+    //-------------- view stuff -------------
     @BindView(R.id.editOldPasswordSection)
     ConstraintLayout editOldPassword;
 
@@ -21,6 +34,11 @@ public class EditAccountActivity extends AppCompatActivity {
 
     @BindView(R.id.passwortFurther)
     Button further;
+
+    //--------------- edit stuff --------------
+    @BindView(R.id.oldPasswordText)
+    EditText oldPasswordText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +49,38 @@ public class EditAccountActivity extends AppCompatActivity {
         editAccount.setVisibility(View.GONE);
     }
 
+    /**
+     * functionality of the further button:
+     *   - tries to log the user in
+     *   - then sets the editAccountSection on visible
+     *   - sets itself and the editOldPasswortSeciton in gone
+     */
     @OnClick(R.id.passwortFurther)
     public void further(){
-        if(validateOldPassword()){
-            editOldPassword.setVisibility(View.GONE);
-            editAccount.setVisibility(View.VISIBLE);
-            further.setVisibility(View.GONE);
-        }
+        String password = oldPasswordText.getText().toString();
+
+        FirebaseAuth fireAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = fireAuth.getCurrentUser();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(user.getEmail(), password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.i(TAG, "Login successful");
+                    editOldPassword.setVisibility(View.GONE);
+                    editAccount.setVisibility(View.VISIBLE);
+                    further.setVisibility(View.GONE);
+                } else {
+                    Log.e(TAG, "Login failed");
+                    Toast.makeText(getApplicationContext(), getString(R.string.ErrorLoginPasswordWrong), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
+    /**
+     * closes the activity
+     */
     @OnClick(R.id.accountEditClose)
     public void close(){
         Intent intent = new Intent(this, SettingsActivity.class);
@@ -47,7 +88,26 @@ public class EditAccountActivity extends AppCompatActivity {
         finish();
     }
 
-    private boolean validateOldPassword(){
-        return true;
+    /**
+     * functionality of the email complete button
+     *   - checks if the new email is already taken
+     *   - checks if email is valid
+     *   - saves the new email in authentication
+     *   - saves the new email on the database
+     */
+    @OnClick(R.id.emailComplete)
+    public void editEmail(){
+
+    }
+
+    /**
+     * functionality of the password complete button
+     *   - checks if it really is a new password
+     *   - checks if new password is valid
+     *   - saves new password in authentication
+     */
+    @OnClick(R.id.passwortComplete)
+    public void editPasswort(){
+
     }
 }
