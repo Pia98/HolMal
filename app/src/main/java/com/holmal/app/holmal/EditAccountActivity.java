086f45ecca6ed82819e6fc81fa0d049ed3807817
Pia Probst <pia.probst@online.de>
2019-02-03 16:36:07 +0100

    03-02-2019 16:35 - first functionality

--- app/src/main/java/com/holmal/app/holmal/EditAccountActivity.java
+++ app/src/main/java/com/holmal/app/holmal/EditAccountActivity.java
@@ -1,12 +1,15 @@
 package com.holmal.app.holmal;
 
+import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class EditAccountActivity extends AppCompatActivity {
 
@@ -16,6 +19,9 @@ public class EditAccountActivity extends AppCompatActivity {
     @BindView(R.id.editAccountSection)
     ConstraintLayout editAccount;
 
+    @BindView(R.id.passwortFurther)
+    Button further;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +29,25 @@ public class EditAccountActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         editAccount.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.passwortFurther)
+    public void further(){
+        if(validateOldPassword()){
+            editOldPassword.setVisibility(View.GONE);
+            editAccount.setVisibility(View.VISIBLE);
+            further.setVisibility(View.GONE);
+        }
+    }
+
+    @OnClick(R.id.accountEditClose)
+    public void close(){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private boolean validateOldPassword(){
+        return true;
     }
 }
