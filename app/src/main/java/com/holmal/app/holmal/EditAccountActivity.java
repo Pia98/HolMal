a43af08bf9f97654daf9a1998db001ce62e1de48
Pia Probst <pia.probst@online.de>
2019-02-03 16:29:28 +0100

    03-02-2019 16:29 - styling

--- app/src/main/java/com/holmal/app/holmal/EditAccountActivity.java
+++ app/src/main/java/com/holmal/app/holmal/EditAccountActivity.java
@@ -1,13 +1,27 @@
 package com.holmal.app.holmal;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class EditAccountActivity extends AppCompatActivity {
+
+    @BindView(R.id.editOldPasswordSection)
+    ConstraintLayout editOldPassword;
+
+    @BindView(R.id.editAccountSection)
+    ConstraintLayout editAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_account);
+        ButterKnife.bind(this);
+
+        editAccount.setVisibility(View.GONE);
     }
 }
